51423c9ab9661638b07445de45966026cd8e8cfa
Andinet Asamnew <andyasne@gmail.com>
2022-04-14 15:14:57 +0300

    Added DraggableScrollableSheet

--- flutter_draggablescrollablesheet/lib/main.dart
+++ flutter_draggablescrollablesheet/lib/main.dart
@@ -99,42 +99,60 @@ class _MyHomePageState extends State<MyHomePage> {
                     enableDrag:
                         true, // parameter specifies whether the bottom sheet can be dragged up and down and dismissed by swiping downwards.
                     isDismissible:
-                        true, // specifies whether the bottom sheet will be dismissed when user taps on the scrim.
+                        false, // specifies whether the bottom sheet will be dismissed when user taps on the scrim.
                     barrierColor: Colors.transparent,
                     builder: (context) {
-                      return Column(
-                        mainAxisSize: MainAxisSize.max,
-                        children: <Widget>[
-                          ListTile(
-                            leading: new Icon(Icons.photo),
-                            title: new Text('Photo'),
-                            onTap: () {
-                              Navigator.pop(context);
-                            },
-                          ),
-                          ListTile(
-                            leading: new Icon(Icons.music_note),
-                            title: new Text('Music'),
-                            onTap: () {
-                              Navigator.pop(context);
-                            },
-                          ),
-                          ListTile(
-                            leading: new Icon(Icons.videocam),
-                            title: new Text('Video'),
-                            onTap: () {
-                              Navigator.pop(context);
-                            },
-                          ),
-                          ListTile(
-                            leading: new Icon(Icons.share),
-                            title: new Text('Share'),
-                            onTap: () {
-                              Navigator.pop(context);
-                            },
-                          ),
-                        ],
-                      );
+                      return DraggableScrollableSheet(
+                          expand:
+                              false, //This field specifies whether the widget should expand to fill the available space in its parent or not. The default value is true. We have not specified because we want it to be true.
+                          initialChildSize:
+                              0.6, //This field specifies the initial size of the bottom draggable sheet you want to appear on the fraction of the screen and takes a double value. Its default value is 0.5. Its value range from 0 – 1.0.
+                          minChildSize:
+                              0.5, //This field specifies the minimum size of the DraggableScrollableSheet widget that is when any user will scroll down to close the widget, the minimum height will appear. Its default value is 0.25 and ranges from 0 – 1.0.
+                          maxChildSize:
+                              0.9, // This field specifies the maximum size of the DraggableScrollableSheet widget that is when any user will scroll up to open the widget, the maximum height will appear. Its default value is 1.0 and ranges from 0 – 1.0. It also specifies the fraction of the screen to be occupied.
+                          builder: (_, scrollController) {
+                            return Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                ),
+                                child: Stack(children: [
+                                  Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: <Widget>[
+                                        ListTile(
+                                          leading: new Icon(Icons.photo),
+                                          title: new Text('Photo'),
+                                          onTap: () {
+                                            Navigator.pop(context);
+                                          },
+                                        ),
+                                        ListTile(
+                                          leading: new Icon(Icons.music_note),
+                                          title: new Text('Music'),
+                                          onTap: () {
+                                            Navigator.pop(context);
+                                          },
+                                        ),
+                                        ListTile(
+                                          leading: new Icon(Icons.videocam),
+                                          title: new Text('Video'),
+                                          onTap: () {
+                                            Navigator.pop(context);
+                                          },
+                                        ),
+                                        ListTile(
+                                          leading: new Icon(Icons.share),
+                                          title: new Text('Share'),
+                                          onTap: () {
+                                            Navigator.pop(context);
+                                          },
+                                        ),
+                                      ])
+                                ]));
+                          });
                     });
               },
               padding:
