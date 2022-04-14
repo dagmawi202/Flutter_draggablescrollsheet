93aa65f9792c97cce421cfd998c81dfe2f34d7be
Andinet Asamnew <andyasne@gmail.com>
2022-04-14 14:35:36 +0300

    showModalBottomSheet

---
+++ flutter_draggablescrollablesheet/lib/comment_sheet.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+    
+class CommentSheet extends StatefulWidget {
+  const CommentSheet({Key? key}) : super(key: key);
+
+  @override
+  _CommentSheetState createState() => _CommentSheetState();
+}
+
+class _CommentSheetState extends State<CommentSheet> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
